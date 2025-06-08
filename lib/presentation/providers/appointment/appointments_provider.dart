358befe19cc1b103376a181b54d51714706ab870
juankkay010 <juanandrescuervoaguirre8@gmail.com>
2025-06-07 20:49:59 -0500

    refactor: se cambio appointments para usar repository y repository impl

--- lib/presentation/providers/appointment/appointments_provider.dart
+++ lib/presentation/providers/appointment/appointments_provider.dart
@@ -1,8 +1,28 @@
+import 'package:barber/data/datasources/mock_appointments_datasource.dart';
+import 'package:barber/data/repositories_impl/appointments_repository_impl.dart';
+import 'package:barber/domain/repositories/appointments_repository.dart';
 import 'package:barber/domain/usecases/get_appointments_by_date.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 import 'appointments_notifier.dart';
 import 'appointments_state.dart';
 
-final appointmentsProvider = StateNotifierProvider<AppointmentsNotifier, AppointmentsState>(
-  (ref) => AppointmentsNotifier(GetAppointmentsByDate()),
-);
+// Datasource provider
+final appointmentsDatasourceProvider = Provider<MockAppointmentsDatasource>((ref) {
+  return MockAppointmentsDatasource();
+});
+
+// Repository provider
+final appointmentsRepositoryProvider = Provider<AppointmentsRepository>((ref) {
+  return AppointmentsRepositoryImpl(ref.watch(appointmentsDatasourceProvider));
+});
+
+// Use case provider
+final getAppointmentsByDateProvider = Provider<GetAppointmentsByDate>((ref) {
+  return GetAppointmentsByDate(ref.watch(appointmentsRepositoryProvider));
+});
+
+// Notifier provider
+final appointmentsProvider = StateNotifierProvider<AppointmentsNotifier, AppointmentsState>((ref) {
+  return AppointmentsNotifier(ref.watch(getAppointmentsByDateProvider));
+});
